3fe526fa69c34f1f372569be94aecbf8fb872fe0
  <>
2021-02-09 13:19:49 +0200

    r**2-termi myös tallennukseen

--- laske_gumbkertoimet.py
+++ laske_gumbkertoimet.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-#tallentaa kustakin ajosta aikasarjan, jossa on kolmella sarakkeella on:
+#tallentaa kustakin ajosta aikasarjan, jossa on neljällä sarakkeella on:
 #0. kertymäfunktion sovitussuoran kulmakerroin gumbelkoordinaatistossa
 #1. suoran vakiotermi
-#2. vuosiluku kyseisen aikaikkunan puolivälistä
+#2. r^2 eli korrelaatiokertoimen neliö
+#3. vuosiluku kyseisen aikaikkunan puolivälistä
 
 import numpy as np
 import scipy.stats as st
@@ -43,7 +44,7 @@ for aind in range(len(ajot)):
 
         #suoran sovitus sekä suoran parametrien ja vuoden tallentaminen
         a, b, r, p, kkv = st.linregress(pa[0:raja], F[0:raja]);
-        f.write("%.5e\t%.5f\t%i\n" %(a,b, v0+ind+(n-1)//2));
+        f.write("%.5e\t%.5f\t%.4f\t%i\n" %(a,b,r**2, v0+ind+(n-1)//2));
 
         ind += 1;
         if (ind+n > len(data)):
