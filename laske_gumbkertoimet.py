e718b4d8e0b1f1e71abe512eab33f49857b629d2
  <>
2021-02-09 13:03:07 +0200

    Suoran parametrien aikasarjan laskeminen omaksi ohjelmaksi

---
+++ laske_gumbkertoimet.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+#tallentaa kustakin ajosta aikasarjan, jossa on kolmella sarakkeella on:
+#0. kertymäfunktion sovitussuoran kulmakerroin gumbelkoordinaatistossa
+#1. suoran vakiotermi
+#2. vuosiluku kyseisen aikaikkunan puolivälistä
+
+import numpy as np
+import scipy.stats as st
+
+sk = '/home/aerkkila/a/pintaalat_15_1/';
+uk = '/home/aerkkila/a/kuvat1/';
+
+v0 = 2006; #ensimmäinen vuosi datassa
+ajot = ["A002", "A005", "B002", "B005", "D002", "D005"];
+nimet = ["A_RCP4.5", "A_RCP8.5", "B_RCP4.5", "B_RCP8.5", "D_RCP4.5", "D_RCP8.5"];
+n = 30; #aikaikkunan pituus; jos negatiivinen, otetaan kaikki vuodet
+
+for aind in range(len(ajot)):
+    data = np.genfromtxt(sk + 'pa_' + ajot[aind] + '_maks.txt', usecols=[0]);
+    if(n < 0):
+        n = len(data);
+    
+    #muodostetaan juokseva aikasarja, jossa on kerrallaan mukana n vuotta
+    #kun v on aikaikkunan 1. vuosi, keskiarvon vuodeksi laitetaan v+n/2
+    #kun n on parillinen, takaa jää yksi vuosi vähemmän pois kuin edestä
+    ind = 0;
+
+    f = open("gumbkertoimet_" + ajot[aind] + ".txt", "w");
+    while 1:
+        pa = data[ind : ind+n]; #valitaan aikaikkuna
+        pa = np.sort(pa);
+        F = np.array(range(1,len(pa)+1)) / (len(pa)+1.0); #kokeellinen kertymäfunktio
+
+        #rajataan ei-huomioitaviksi suoran sovituksessa kokonaisjäätymiset
+        raja = len(pa);
+        for tmp in range(len(pa)-1):
+            if(pa[tmp] > 103000):
+                raja = tmp;
+                break;
+
+        F = -1*np.log(-1*np.log(F)); #muunnos gumbelkoordinaatistoon
+
+        #suoran sovitus sekä suoran parametrien ja vuoden tallentaminen
+        a, b, r, p, kkv = st.linregress(pa[0:raja], F[0:raja]);
+        f.write("%.5e\t%.5f\t%i\n" %(a,b, v0+ind+(n-1)//2));
+
+        ind += 1;
+        if (ind+n > len(data)):
+            break;
+    f.close();
