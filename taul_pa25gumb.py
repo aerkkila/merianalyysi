909628720e176323ee23cc7a65109c450bf91469
  <>
2021-02-23 00:41:08 +0200

    vanha taulukonteko-ohjelma päivitetty

---
+++ taul_pa25gumb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+#tekee taulukon gumelsovituksen r²-termeistä pienimmässä neljänneksessä
+
+import numpy as np
+import scipy.stats as st
+
+sk = '/home/aerkkila/a/pintaalat_15_1/';
+uk = '/home/aerkkila/a/';
+
+ajot = ["A002", "A005", "B002", "B005", "D002", "D005"];
+nimet = ("Max Planck", "EC-Earth", "Hadley Center");
+
+f = open(uk + "taul_pa25gumb151.txt", "w");
+f.write("& RCP 4.5 & RCP 8.5 \\\\\n\\hline\n");
+for aind in range(len(ajot)):
+    data = np.genfromtxt(sk + 'pa_' + ajot[aind] + '_maks.txt');
+    i = 0;
+    pa = data[:,0];
+    vuodet = data[:,2];
+    pa = np.sort(pa);
+    F = np.array(range(1,len(pa)+1)) / (len(pa)+1.0);
+
+    F = -1*np.log(-1*np.log(F));
+    raja = len(pa)//4;
+    a, b, r, p, kkv = st.linregress(pa[0:raja], F[0:raja]);
+
+    if(aind % 2 == 0):
+        f.write("%s & %.3f & " %(nimet[aind//2], r**2));
+    else:
+        f.write("%.3f \\\\\n" %r**2);
+
+f.close();
