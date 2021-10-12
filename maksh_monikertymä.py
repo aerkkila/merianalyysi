14ef3374c299738b40536e31d162c934b7397755
  <>
2021-10-12 11:31:47 +0300

    lisätty lajeihin ø joka tässä tulkitaan weibullina

--- maksh_monikertymä.py
+++ maksh_monikertymä.py
@@ -27,10 +27,10 @@ if len(lajit) > 3 and lajit[-4:] == '.txt':
         tied = f.read()
     lajit = ''
     for c in tied:
-        if c == 'w' or c == 'g' or c == 'f':
+        if c == 'w' or c == 'g' or c == 'f' or c == 'ø':
             lajit += c
     if(len(lajit) != len(ajot)*len(paikat)):
-        printf("Varoitus, luettiin %i lajia %i:n sijaan" %(len(lajit), len(ajot)*len(paikat)))
+        print("Varoitus, luettiin %i lajia %i:n sijaan" %(len(lajit), len(ajot)*len(paikat)))
 
 def valitse_yhtalot(laji):
     global Fm, xm, Fpalaute
@@ -42,7 +42,7 @@ def valitse_yhtalot(laji):
         Fm = lambda F: log(-log(F))
         xm = lambda x: log(x+1e-7)
         Fpalaute = lambda x: exp(-x**a*exp(b))
-    elif laji == 'w': #weibull
+    elif laji == 'w' or laji == 'ø': #weibull
         Fm = lambda F: log(-log(1-F))
         xm = lambda x: log(x+1e-7)
         Fpalaute = lambda x: 1-exp(-x**a*exp(b))
@@ -66,7 +66,7 @@ for pind,paikka in enumerate(paikat):
 
         raja = len(suure)
         for i,s in enumerate(suure):
-            if(suure[i] > 1): #aivan pieniä paksuuksia ei huomioida
+            if(s > 1): #aivan pieniä paksuuksia ei huomioida
                 raja = i
                 break
 
@@ -76,12 +76,11 @@ for pind,paikka in enumerate(paikat):
         x1 = xm(suure)
         if sovitus == 'pns':
             if 1:
-                a,b,r,p,kkv = st.linregress(F1[raja:], x1[raja:]) #sovitetaan väärin päin, jotta neliösumma minimoidaan x-suunnassa
+                a,b,r,p,kkv = st.linregress(F1[raja:], x1[raja:]) # minimoidaan neliösumma x-suunnassa
                 b = -b/a
                 a = 1/a
             else:
                 a,b,r,p,kkv = st.linregress(x1[raja:], F1[raja:])
-            print(a,b)
         elif sovitus == 'ts':
             ts = st.mstats.theilslopes(F1[raja:], x1[raja:])
             a = ts[0]
