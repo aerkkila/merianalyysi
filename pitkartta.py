431ce8acd3e88e9b9763eecd9e3d7c9e18ecd97a
  <>
2021-10-22 15:52:05 +0300

    väriskaala vaihdettu

--- pitkartta.py
+++ pitkartta.py
@@ -3,6 +3,7 @@
 import struct
 from jaettu import ajonimet
 from matplotlib.pyplot import *
+from matplotlib.cm import get_cmap
 import numpy as np
 
 for jasen in ['10', '50', '90']:
@@ -21,10 +22,13 @@ for jasen in ['10', '50', '90']:
         muoto = 'h'*xpit
         for j in range(ypit):
             kuva[ypit-1-j,:] = struct.unpack(muoto, sisalto[8+j*xpit*2:8+(j+1)*xpit*2])
-        imshow(kuva)
+        imshow(kuva,cmap=get_cmap('magma'))
         title(ajonimet[kind*2][:-len(' M.N')])
         clim(0,225)
         axis(False)
     alue = fig.add_axes((0.9,0.1,0.05,0.8))
     colorbar(cax=alue)
-    show()
+    if(sys.argv[-1] == '1'):
+        savefig("pituus%s.png" %jasen)
+    else:
+        show()
