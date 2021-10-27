be7a3cb90cbbd5b160c0d95bb64fa9a6d3857a3f
  <>
2021-10-27 14:07:58 +0300

    Tekee onnistuneesti kuvaajan

--- hKert_hist.py
+++ hKert_hist.py
@@ -1,51 +1,60 @@
 #!/usr/bin/python3
 
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
 import numpy as np
+import pandas as pd
 from scipy.stats import *
 from jaettu import *
 import locale, sys
 import matplotlib.ticker as ticker
 
-locale.setlocale(locale.LC_ALL, paikallisuus);
+#locale.setlocale(locale.LC_ALL, paikallisuus);
 
-paikkaind = (2, 8, 6, 11);
+#paikkaind = (2, 8, 6, 11);
+#nimet = ("Tornio (Röyttä)", "Kemi (Ajos)", "Hailuoto (Marjaniemi)", "Raahe", "Ratan", "Nordmaling (Järnäs)", "Kalajoki (Rahja)", "Kokkola (Märaskär)", "Mustasaari (Vallgrund)")
+nimet = ("Tornio", "Kemi", "Hailuoto", "Raahe", "Kalajoki", "Kokkola", "Korsholm", "Bygdeå", "Nordmaling")
+tnimet = ("Tornio", "Kemi", "Hailuoto", "Raahe", "Kalajoki", "Kokkola", "Mustasaari", "Bygdeå", "Nordmaling")
 
-#luetaan havainnot
-hnnot = [[]]*(loppuvuosi-alkuvuosi);
-data=np.genfromtxt(hnnotTied, delimiter=",", comments="#", usecols=paikkaind);
-for i in range(len(paikkaind)):
-    v0 = alkuvuosi;
-    v1 = loppuvuosi;
-    if(paikat[i] == "Nordmaling"): #täältä on eri aikasarja
-        v0 += 1;
-        v1 += 1;
-    H = data[v0-1912 : v1-1912, i] #valitaan aika ja paikka
-    H = H[~np.isnan(H)];
-    hnnot[i] = H;
+v0 = (1978,1975,1975,1978,1975,1978,1975,1974,1976)
+v1 = (2008,2005,2005,2008,2005,2008,2005,2007,2006)
 
-#luetaan malli
-paikka_ajo = [[]]*len(paikat);
-for j in range(len(paikat)):
-    paikkatulos = [[]]*len(ajot);
-    for i in range(len(ajot)):
-        tied = "%s%s_%s_%s_maks.txt" %(sk, paikat[j], muuttuja, ajot[i]);
-        paikkatulos[i] = np.genfromtxt(tied, usecols=(0));
+hnnot = pd.read_csv('../perämeri.csv', index_col=0)
 
-    paikka_ajo[j] = np.vstack(paikkatulos);
-    
-plt.figure(figsize=(12,10));
+fig = figure(figsize=(12,12))
+axs = fig.subplots(3,3).flatten()
+ytiedos = np.arange(1,32) / 32
+for pind,paikka in enumerate(nimet):
+    sca(axs[pind])
+    print(paikka)
+    for aind,ajo in enumerate(ajot_hist):
+        tied = "%s/maksh_hist_%s_%s.txt" %(kansio, tnimet[pind], ajo);
+        tiedos = np.loadtxt(tied, usecols=(0));
+        tiedos = np.sort(tiedos)
+        plot(tiedos, ytiedos, color=varit[aind*2], label=ajonimet_hist[aind])
+        print(tiedos[np.where(tiedos > 100)])
+    print(v0[pind])
+    print(v1[pind])
+    hnto = hnnot[paikka][np.arange(v0[pind],v1[pind]+1)]
+    hnto = np.sort(hnto)
+    hnto = hnto[np.where(~np.isnan(hnto))]
+    plot(hnto, ytiedos, color='y', label='observations')
+    legend(fontsize=10,frameon=False)
+    grid('on')
+    title(paikka)
+show()
+exit()
+
 
 for p in range(len(paikat)):
     ax = plt.subplot(3,2,p+1);
     
     if(p < len(paikkaind)): #havainnot
         htmp = np.sort(hnnot[p]);
-        F = np.array(range(1,len(htmp)+1)) / (len(htmp)+1.0); #kokeellinen kertymäfunktio
+        F = np.array(range(1,len(htmp)+1)) / (len(htmp)+1.0); #diskreetti kertymäfunktio
         plt.plot(htmp, F, color='k', label="havainnot");        
     for a in range(len(ajot)): #malli
         htmp = np.sort(paikka_ajo[p][a])
-        F = np.array(range(1,len(htmp)+1)) / (len(htmp)+1.0); #kokeellinen kertymäfunktio
+        F = np.array(range(1,len(htmp)+1)) / (len(htmp)+1.0); #diskreetti kertymäfunktio
         plt.plot(htmp, F, color=varit[a], label=ajonimet[a]);
     
     plt.grid('on')
