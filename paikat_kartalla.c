e9f67579b43a780cc1416aceb56dd447e607b6be
  <>
2021-10-27 12:15:21 +0300

    karttaohjelma toimii yleisemmin

--- paikat_kartalla.c
+++ paikat_kartalla.c
@@ -6,31 +6,42 @@
 const char* nctiedosto = "/scratch/project_2002540/siiriasi/smartsea_data/D005/NORDIC-GOB_1d_20950101_20951231_grid_T.nc";
 const char* ncmuuttuja = "SST";
 const int luettava_paiva = 212; //31.7.
-char* paikat[] = {"Kemi", "Kalajoki", "Mustasaari", "Nordmaling", "Rauma", "Söderhamn"};
+//char* paikat[] = {"Kemi", "Kalajoki", "Mustasaari", "Nordmaling", "Rauma", "Söderhamn"};
 const int lpit = 500;
 
 #define NCFUNK(funk, ...) {if((ncpalaute = funk(__VA_ARGS__)))	\
       printf("Virhe: %s\n", nc_strerror(ncpalaute));}
 
-int main() {
+int main(int argc, char** argv) {
   /*luetaan paikkojen indeksit koordinaatistossa tiedostosta koordinatit.txt*/
-  FILE *f = fopen("koordinaatit.txt", "r");
-  char* rivi = malloc(lpit);
+  if(argc < 2) {
+    printf("Käyttö: ./tämä koordinaatit.txt\n");
+    exit(0);
+  }
+  FILE *f = fopen(argv[1], "r");
+  char mluenta[lpit];
   const char haku[] = "indeksi = ";
-  int indeksit[6];
-  for(int i=0; i<6; i++) {
-    fscanf(f, "%s", rivi);
-    rivi[strlen(rivi)-1] = 0;
-    if(strcmp(rivi, paikat[i]))
-      printf("Luettiin: %s, piti olla %s\n", rivi, paikat[i]);
-    fscanf(f, "%*[^\n]\n");
-    fgets(rivi, lpit, f);
-    char* kohta = strstr(rivi, haku) + strlen(haku);
+  //  char* paikat[50];
+  int indeksit[50];
+  int paikkoja=-1;
+  for(int i=0; i<50; i++) {
+    if(fscanf(f, "%s", mluenta) < 1) {
+      paikkoja = i;
+      break;
+    }
+    mluenta[strlen(mluenta)-1] = 0; // Kemi: --> Kemi
+    //paikat[i] = strdup(mluenta);
+    fscanf(f, "%*[^\n]\n"); //indeksi on seuraavalla rivillä
+    fgets(mluenta, lpit, f);
+    char* kohta = strstr(mluenta, haku) + strlen(haku);
     if(sscanf(kohta, "%i", indeksit+i) != 1)
-      printf("Ei luettu indeksiä (%s: %s)\n", paikat[i], kohta);
+      printf("Ei luettu indeksiä rivillä %i*2 kohdasta %s\n", i, kohta);
   }
   fclose(f);
-  free(rivi);
+  if(paikkoja == -1) {
+    printf("Varoitus: Paikkoja oli enemmän kuin oli varauduttu lukemaan. Kaikkia ei luettu.\n");
+    paikkoja=50;
+  }
   
   /*koon lukeminen*/
   int ncid, id, ncpalaute;
@@ -53,7 +64,7 @@ int main() {
   char* kartta = malloc(xy);
   for(size_t i=0; i<xy; i++)
     kartta[i] = luenta[i] > 0 && luenta[i] < 100; //merkitään merialue
-  for(int i=0; i<6; i++)
+  for(int i=0; i<paikkoja; i++)
     kartta[indeksit[i]] = i+2; //merkitään paikat
 
   int32_t koko[] = {(int32_t)xpit, (int32_t)ypit};
