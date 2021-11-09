554c921ac56b1b173a5be040b3af1a5604ea3016
  <>
2021-11-09 16:17:59 +0200

    Toimii

--- jääkartat.c
+++ jääkartat.c
@@ -8,10 +8,10 @@
 
 const char* lahdekansio = "/home/aerkkila";
 
-const double r        = 6371229;
+const double R2      = 40592558970441; //(6371229 m)^2
 const float latraja  = 60.2;
 #ifndef KONSRAJA
-#define KONSRAJA 0.0
+#define KONSRAJA 0.01 //prosentteina
 #endif
 #define NCVIRHE(arg) do {printf("Virhe: %s\n", nc_strerror(arg)); exit(1);} while(0)
 #define NCFUNK(fun, ...)			\
@@ -24,8 +24,6 @@ const float latraja  = 60.2;
 const int ypit2=3120, xpit2=2640;
 const int xpit0=700; //muutettaessa huomioitakoon myös countpt
 const size_t countpt0[] = {1, 1, 700};
-//const size_t countpt1[] = {2640, 1520};
-//size_t alkupiste, loppupiste;
 int ypit0, yalku0=0;
 int ncpalaute;
 float *lat, *lon, *lat0;
@@ -41,16 +39,14 @@ void maarita_i_alut(int* i_alku);
 void lue_muuttuja0(int ncid, int* i_alku);
 void laske_pintaalat(int* i_alku);
 double maarita_laajuus();
+int maarita_paiva(const char* restrict nimi, int* paiva, int* vuosi);
 
 /*Tarvittavat koot ovat
   xypit2: koko kartan koko
   xypit0: se osa, joka todellisuudessa tarvitaan. Tämä on viimeisenä määritettävä tarkin arvo.*/
 int main(int argc, char** argv) {
   char apuc[300], *apuc1;
-  int vuosi0=1980, vuosi1=2010;
-  //alkupiste  = yalku1*xpit2;
-  //loppupiste = yloppu1*xpit2;
-  //size_t xypit1 = loppupiste-alkupiste;
+  int vuosi0=1980, vuosi1=2011;
   size_t xypit2 = xpit2*ypit2;
   
   /*mahdolliset komentoriviargumentit*/
@@ -87,15 +83,55 @@ int main(int argc, char** argv) {
   alat= malloc(xypit0*sizeof(double));
   laske_pintaalat(i_alku);
   
-  lue_muuttuja0(ncid, i_alku);
-  double pa = maarita_laajuus();
-  printf("%lf\n", pa/1.0e6);
-
-  NCFUNK(nc_close, ncid);
-  //poista_tiedosto(apuc1);
+  NCFUNK(nc_close, ncid); //silmukka on helpompi, kun nämä suljetaan tässä välissä
   closedir(d);
+
+  int paiv, vuos;
+  for(int vuosi=vuosi0; vuosi<vuosi1; vuosi++) {
+    d = avaa_vuosi(vuosi, apuc);
+    if(!d)
+      break;
+    while(1) {
+      if(pura_seuraava(d, apuc, apuc1))
+	break;
+      ncid = avaa_netcdf(apuc1);
+      //tarkista_koordinaatit(ncid);
+      lue_muuttuja0(ncid, i_alku);
+      double pa = maarita_laajuus();
+      if(maarita_paiva(apuc1, &paiv, &vuos)) {
+	printf("Virheellinen tiedostonnimi: \"%s\"\n", apuc1);
+	exit(1);
+      }
+      printf("%lf\t%i\t%i\n", pa, paiv, vuos);
+      NCFUNK(nc_close, ncid);
+      //poista_tiedosto(apuc1);
+    }
+    closedir(d);
+    d = NULL;
+  }
+  if(d)
+    closedir(d);
   free(lat); free(lon); free(var); free(alat);
   fclose(f_ulos);
+  return 0;
+}
+
+int maarita_paiva(const char* restrict nimi, int* paiva, int* vuosi) {
+  int kk;
+  static int vuosi365[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}; //kuukauden alussa kuluneet päivät
+  static int vuosi366[] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+  if(sscanf(nimi, "%4d%2d%2d", vuosi, &kk, paiva) != 3)
+    return 1;
+  if(*vuosi%4) {
+    *paiva = vuosi365[kk-1] + *paiva-1; //kuukauden päivästä vuoden päiväksi
+    int tmp = *paiva; //ilman tätä tämä ei toimi, vaikka nähdäkseni pitäisi
+    *paiva = (tmp+122) % 365 - 122; //vähintään 243 pyörähtää ympäri ja palaa negatiiviseksi
+  } else {
+    *paiva = vuosi366[kk-1] + *paiva-1;
+    int tmp = *paiva;
+    *paiva = (tmp+122) % 366 - 122;
+  }
+  *vuosi += (*paiva<0);
   return 0;
 }
 
@@ -103,7 +139,7 @@ DIR* avaa_vuosi(int vuosi, char* polku) {
   sprintf(polku, "%s/%i/", lahdekansio, vuosi);
   DIR *d = opendir(polku);
   if(!d) {
-    fprintf(stderr, "Kansiota \"%s\" ei voitu avata\n", polku);
+    printf("Kansiota \"%s\" ei voitu avata\n", polku);
     return NULL;
   }
   return d;
@@ -181,7 +217,7 @@ void lue_muuttuja0(int ncid, int* i_alku) {
   }
 }
 
-#define PINTAALA(lat1, lat2, lon1, lon2) (((lon2)-(lon1))*r*r*(sinf(lat2)-sinf(lat1))*1.0e-6)
+#define PINTAALA(lat1, lat2, lon1, lon2) (((lon2)-(lon1))*R2*(sinf(lat2)-sinf(lat1))*1e-6)
 #define RAD(a) ((a)*0.017453293)
 
 void laske_pintaalat(int* i_alku) {
@@ -202,10 +238,12 @@ void laske_pintaalat(int* i_alku) {
 #endif
 }
 
+#define IND j*xpit0+i
 double maarita_laajuus() {
   double pa = 0;
   for(int j=0; j<ypit0-1; j++)
     for(int i=0; i<xpit0-1; i++)
-      pa += (var[j*xpit0+i] >= KONSRAJA) * var[j*xpit0+i];
+      pa += (var[IND]>=KONSRAJA) * (var[IND]<1000) * alat[IND]; //1000 poistaa täyttöarvon
   return pa;
 }
+#undef IND
