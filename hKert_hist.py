2276ed65129e7d09949c3ae1d49ccb402e694b56
  <>
2021-11-22 14:58:37 +0200

    lisätty pituuskartta yksinään

--- hKert_hist.py
+++ hKert_hist.py
@@ -65,7 +65,6 @@ legend()
 grid('on')
 xlim((0,120))
 xlabel('Maximum thickness (cm)')
-ylabel('Cumulative probability')
 tight_layout()
 if(sys.argv[-1] == '1'):
     savefig('%s/maksh_kert_hist_kaikki.png' %kuvat)
---
+++ pitkartta_kartta.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import struct, matplotlib.colors as colors
+from jaettu import ajonimet,kuvat
+from matplotlib.pyplot import *
+from matplotlib.cm import get_cmap
+import numpy as np
+
+#ensin tehdään kartat kaikista vuosista ohjelmilla pktied.c ja pitkart_kartoista.c
+#näistä 10 50 90 prosenttiosuus ohjelmalla pitkarttoja.c
+#sitten käytetään tätä
+
+#värikartta halutaan epäjatkuvana
+kartta = get_cmap('gnuplot2')
+raja1=181
+rajat = np.arange(0,raja1,15)
+norm = colors.BoundaryNorm(rajat, kartta.N, clip=True)
+
+Alue = (0, 0, 1, 1)
+
+with open("pituus50_K002.bin", "rb") as f:
+    sisalto = f.read()
+xpit,ypit,v0,v1 = struct.unpack('hhhh', sisalto[0:8])
+fig=figure(figsize=(xpit/100/0.86, ypit/100))
+fig.add_axes(Alue)
+kuva = np.empty((ypit,xpit), dtype=int)
+muoto = 'h'*xpit
+for j in range(ypit):
+    kuva[ypit-1-j,:] = struct.unpack(muoto, sisalto[8+j*xpit*2 : 8+(j+1)*xpit*2])
+imshow(kuva, cmap=kartta)
+clim(0,raja1)
+axis(False)
+
+alue = fig.add_axes((0.88,0.1,0.05,0.8))
+cb = colorbar(cax=alue, ticks=rajat)
+yticks(fontsize=26)
+if(sys.argv[-1] == '1'):
+    savefig("pituuskartta50_kartta.png")
+else:
+    show()
