d34858e4037e74185b401bdc44add0d4bcd36964
  <>
2021-11-23 10:06:28 +0200

    yleistetty ja nimi vaihdettu

--- kartta_yksi.py
+++ kartta_yksi.py
@@ -5,6 +5,7 @@ from jaettu import ajonimet,kuvat
 from matplotlib.pyplot import *
 from matplotlib.cm import get_cmap
 import numpy as np
+import sys
 
 #ensin tehdään kartat kaikista vuosista ohjelmilla pktied.c ja pitkart_kartoista.c
 #näistä 10 50 90 prosenttiosuus ohjelmalla xkarttoja.c
@@ -17,7 +18,7 @@ norm = colors.BoundaryNorm(rajat, kartta.N, clip=True)
 
 Alue = (0, 0, 1, 1)
 
-with open("ensijää10_A001.bin", "rb") as f:
+with open(sys.argv[1], "rb") as f:
     sisalto = f.read()
 xpit,ypit,v0,v1 = struct.unpack('hhhh', sisalto[0:8])
 fig=figure(figsize=(xpit/100/0.86*2, ypit/100*2))
@@ -35,6 +36,6 @@ alue = fig.add_axes((0.88,0.1,0.05,0.8))
 cb = colorbar(cax=alue, ticks=rajat)
 yticks(fontsize=16)
 if(sys.argv[-1] == '1'):
-    savefig("pituuskartta50_kartta.png")
+    savefig("väliaikainen_nimi.png")
 else:
     show()
